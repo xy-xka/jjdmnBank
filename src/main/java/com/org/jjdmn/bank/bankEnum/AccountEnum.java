7322fd7be09425c18faabfe13263f2eacbbae982
huxf <huxf@hundsun.com>
2021-07-30 11:23:17 +0800

    状态枚举

---
+++ src/main/java/com/org/jjdmn/bank/bankEnum/AccountEnum.java
@@ -0,0 +1,48 @@
+package com.org.jjdmn.bank.bankEnum;
+
+/**
+ * @PackageUrl com.org.jjdmn.bank.bankEnum
+ * @ClassName AccountEnum
+ * @Description TODO
+ * @Author huwx
+ * @Date 2021/7/30
+ * @Version 1.0
+ **/
+public enum AccountEnum {
+
+    //正常账户
+    normal(1,"正常"),
+
+    //冻结账户
+    frozen(2,"冻结"),
+
+    //销户
+    cancellation(3,"销户"),
+
+    //挂失
+    loss(4,"挂失");
+
+    private Integer code;  //状态码
+    private String desc;    //描述
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    AccountEnum(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
---
+++ src/main/java/com/org/jjdmn/bank/bankEnum/TransactionEnum.java
@@ -0,0 +1,46 @@
+package com.org.jjdmn.bank.bankEnum;
+
+/**
+ * @PackageUrl com.org.jjdmn.bank.bankEnum
+ * @ClassName TransactionEnum
+ * @Description TODO
+ * @Author huwx
+ * @Date 2021/7/30
+ * @Version 1.0
+ **/
+public enum TransactionEnum {
+
+    // 转账成功
+    success(1,"转账成功"),
+
+    //转账失败
+    fail(2,"转账失败"),
+
+    //银行处理
+    handler(3,"银行处理");
+
+
+    private Integer code;  //状态码
+    private String desc;    //描述
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    TransactionEnum(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
